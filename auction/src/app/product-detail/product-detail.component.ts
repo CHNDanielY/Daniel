f6544e9f1c4351fac5219cb6d570cc84ab167e22
daniel <913965635@qq.com>
2018-01-16 21:11:30 +0800

    a

--- auction/src/app/product-detail/product-detail.component.ts
+++ auction/src/app/product-detail/product-detail.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import{ActivatedRoute,Params} from  '@angular/router';
-import {Product,ProductService,Comment} from '../shared/product.service';
+import{ActivatedRoute, Params} from  '@angular/router';
+import {Product, ProductService, Comment} from '../shared/product.service';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-product:Product;
-comments:Comment[];
+product: Product;
+comments: Comment[];
 newRating: number = 5;
-newComment: string = "";
-  constructor(private routeInfo:ActivatedRoute,private productService:ProductService) { }
+newComment: string = "hello";
+isCommentHidden = true;
+  constructor(private routeInfo: ActivatedRoute, private productService:ProductService) { }
 
-  ngOnInit() {
-  	let productId:number=this.routeInfo.snapshot.params["productId"];
-  	this.product=this.productService.getProduct(productId);
-  	this.comments=this.productService.getCommentsForProductId(productId);
-  	// console.log(this.comments)
+  ngOnInit() {let productId: number = this.routeInfo.snapshot.params['productId'];
+    this.product = this.productService.getProduct(productId);
+    this.comments = this.productService.getCommentsForProductId(productId);
+    // console.log(this.comments)
 
+  }
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toDateString(), 'someone', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+      this.product.rating = sum / this.comments.length;
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 
 }
--- auction/src/app/stars/stars.component.ts
+++ auction/src/app/stars/stars.component.ts
@@ -1,32 +1,40 @@
-import { Component, OnInit,Input } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
-private stars:boolean[];
+export class StarsComponent implements OnInit, OnChanges {
+
+    private stars: boolean[];
 
 @Input()
 private rating: number = 0;
 
+@Output()
+private ratingChange: EventEmitter<number>= new EventEmitter();
 @Input()
 private readonly: boolean = true;
-
   constructor() { }
-
+    ngOnChanges(changes: SimpleChanges): void {
+        this.stars = [];
+        for (let i = 1; i <= 5; i++) {
+            this.stars.push(i > this.rating);
+        }
+    }
   ngOnInit() {
-  	this.stars = [];
-  	for (let i = 1; i <= 5; i++) {
+  	  this.stars = [];
+  	  for (let i = 1; i <= 5; i++) {
   	  this.stars.push(i > this.rating);
   	}
   	// this.stars=[true,true,true,true,true];
   }
+
   clickStar(index: number ) {
     if (!this.readonly) {
       this.rating = index + 1;
-      this.ngOnInit();
+      this.ratingChange.emit(this.rating);
     }
 
   }
