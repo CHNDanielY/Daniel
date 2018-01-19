97895d2043027f2cc43a795a17f658d66c14f35b
daniel <913965635@qq.com>
2018-01-19 22:36:47 +0800

    webp

--- segment/webpack.config.js
+++ segment/webpack.config.js
@@ -1,57 +1,54 @@
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry:  path.resolve(__dirname + "/app/main.js"),//已多次提及的唯一入口文件
+    entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/public",//打包后的文件存放的地方
-        filename: "bundle.js"//打包后输出文件的文件名
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
     },
+    devtool: 'none',
     devServer: {
-        contentBase: "./public",//本地服务器所加载的页面所在的目录
-        historyApiFallback: true,//不跳转
-        inline: true,//实时刷新
-        hot:true,
-        open:true
+        contentBase: "./public", //本地服务器所加载的页面所在的目录
+        historyApiFallback: true, //不跳转
+        inline: true,
+        hot: true
     },
     module: {
-        rules:[
-            {
-                test: /(\.jsx|\.js)$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "env", "react","es2015"
-                        ]
-                    }
-                },
-                exclude:/node_modules/
+        rules: [{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader"
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    }, {
-                        loader: "css-loader",
-                        options: {
-                            modules:true,//指定使用css modules
-                            localIdentName: '[name]__[local]--[hash:base64:5]' //指定css的类名格式
-                        }
-
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        modules: true,
+                        localIdentName: '[name]__[local]--[hash:base64:5]'
                     }
-                ]
-            }
-        ]
-    },
+                }, {
+                    loader: "postcss-loader"
+                }]
+            })
+        }
+        ]},
     plugins: [
-        new webpack.BannerPlugin("版权所有，翻版必究"),
+        new webpack.BannerPlugin('版权所有，翻版必究'),
         new HtmlWebpackPlugin({
-            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
-        })
+            template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css")
     ]
-};
+
+
+}
+
+
